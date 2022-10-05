41d81169ee0f0c1aa572b6fe92c21b9cd4af8b25
Cuong Nguyen <cuongnguyen.work8797@gmail.com>
2022-10-05 13:11:36 +0700

    fix: add alt tag into image and head with descript and keywords tag

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.scss';
 import '../languages/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/about/index.js
+++ pages/about/index.js
@@ -3,10 +3,19 @@ import LeftMenu from "@/components/LeftMenu";
 import ExperienceSection from "./ExperienceSection";
 import BannerSection from "./BannerSection";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
-const About = () => {
+const About = ({title, description, keywords}) => {
     return (
-        <div className="container-fluid-md container-xl ">
+        <div className="container-fluid-md container-xl">
+            <Head>
+                <html lang="en" />
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
             <div className="row">
                 {/* Left Menu */}
                 <div className="col-sm-12 col-md-4 col-lg-3 position-relative"><div className="card card-body sticky-menu"><LeftMenu /></div> </div>
@@ -20,9 +29,17 @@ const About = () => {
             </div>
             <div className="row pt-5"><Footer /></div>
         </div>
-
-
     )
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            title: "About Me | Nguyen Hai Cuong Developer",
+            description: "This is CV of Full-stack Developer.",
+            keywords: "Full-Stack Developer, Java Developer, Laravel, NodeJS, React"
+        }
+    }
 }
 
 export default About;
